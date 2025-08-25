5dced4bd6b50662d341ba22653497b6ed5cd3219
zakariafl27 <flafizakaria27@gmail.com>
2025-08-25 23:07:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white hover:text-red-400">
+        🚀 TEST 
+      </h1>
     </div>
-  );
+  )
 }
-
-export default App;
