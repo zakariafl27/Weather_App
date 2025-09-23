dcdda824e954b91e04a61e5f4641284762db90a1
zakariafl27 <flafizakaria27@gmail.com>
2025-09-23 22:42:54 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Navbar from "./Navbar";
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <h1 className="text-4xl font-bold text-white hover:text-red-400">
-        🚀 TEST 
-      </h1>
+    <div>
+      <Navbar />
     </div>
   )
 }
